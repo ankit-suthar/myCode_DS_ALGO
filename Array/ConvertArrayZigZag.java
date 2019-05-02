25c691a0a32d3a2b37ac1994c5366e6e8d6cd664
Ankit Suthar <ankitec_engg@yahoo.com>
2019-05-02 19:51:15 +0530

    Update ConvertArrayZigZag.java

--- Array/ConvertArrayZigZag.java
+++ Array/ConvertArrayZigZag.java
@@ -9,15 +9,8 @@ Output: arr[] = {3, 7, 4, 8, 2, 6, 1}
 Input:  arr[] =  {1, 4, 3, 2}
 Output: arr[] =  {1, 4, 2, 3}
 
-A Simple Solution is to first sort the array. After sorting, exclude the first element, 
-swap the remaining elements in pairs. (i.e. keep arr[0] as it is, swap arr[1] and arr[2], 
-swap arr[3] and arr[4], and so on). Time complexity is O(nlogn) since we need to sort the array first.
-
-We can convert in O(n) time using an Efficient Approach. The idea is to use modified one pass 
-of bubble sort. Maintain a flag for representing which order(i.e. < or >) currently we need. 
-If the current two elements are not in that order then swap those elements otherwise not.
-Let us see the main logic using three consecutive elements A, B, C. Suppose we are processing 
-B and C currently and the current relation is ‘<'. But we have B > C. Since current relation 
+The idea is to use modified one pass of bubble sort. 
+Suppose we are processing B and C currently and the current relation is ‘<'. But we have B > C. Since current relation 
 is ‘<' previous relation must be '>‘ i.e., A must be greater than B. So, the relation is 
 A > B and B > C. We can deduce A > C. So if we swap B and C then the relation is 
 A > C and C < B. Finally we get the desired order A C B
