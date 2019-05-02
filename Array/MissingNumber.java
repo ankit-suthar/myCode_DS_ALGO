93758e1c6604f03f5dd88441a86dce67f51de192
Ankit Suthar <ankitec_engg@yahoo.com>
2019-05-02 19:46:52 +0530

    Update MissingNumber.java

--- Array/MissingNumber.java
+++ Array/MissingNumber.java
@@ -2,12 +2,10 @@
 Find the Missing Number
 
 You are given a list of n-1 integers and these integers are in the range of 1 to n. 
-There are no duplicates in list. One of the integers is missing in the list. Write an 
-efficient code to find the missing integer.
+There are no duplicates in list. One of the integers is missing in the list.
 
-Example :
-I/P    [1, 2, 4, ,6, 3, 7, 8]
-O/P    5
+Input: [1, 2, 4, ,6, 3, 7, 8]
+Output: 5
 */
 package com.array.array;
 
