70f7f4757879a44761590e96124c7033078eefb0
Ankit Suthar <ankitec_engg@yahoo.com>
2019-05-03 18:39:03 +0530

    Add files via upload

---
+++ Array/PrintAllPermutationsOfGivenString.java
@@ -0,0 +1,41 @@
+/*
+A string of length n has n! permutation.
+
+The permutations of string ABC:
+ABC ACB BAC BCA CBA CAB
+*/
+package com.array.array;
+
+public class PrintAllPermutationsOfGivenString {
+	
+	void permute(String str,int l,int h)
+	{
+		if(l==h)
+			System.out.println(str);
+		else
+			for(int i=l;i<=h;i++)
+			{
+				str = swap(str,l,i);
+				permute(str,l+1,h);
+				str = swap(str,l,i);
+			}	
+	}
+	
+	public String swap(String a, int i, int j) 
+  { 
+		char ch[] = a.toCharArray();
+		
+		char temp = ch[i];
+		ch[i] = ch[j];
+		ch[j] = temp;
+		
+		return String.valueOf(ch);
+  }
+
+	public static void main(String[] args) {
+		PrintAllPermutationsOfGivenString permutation = new PrintAllPermutationsOfGivenString();
+		String str = "1234";//"ABC"; 
+    int n = str.length(); 
+    permutation.permute(str, 0, n - 1); 
+	}
+}
