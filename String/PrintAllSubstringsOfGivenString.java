0a1d6f241f64c94a7c7c3fec04632fd58474bad3
Ankit Suthar <ankitec_engg@yahoo.com>
2019-05-16 12:28:22 +0530

    Add files via upload

---
+++ String/PrintAllSubstringsOfGivenString.java
@@ -0,0 +1,41 @@
+/*
+Given a string as an input. We need to write a program that will print all non-empty substrings of that given string.
+
+Examples :
+
+Input :  abcd
+Output :  a 
+          b
+          c
+          d
+          ab
+          bc
+          cd
+          abc
+          bcd
+          abcd
+
+*/
+package com.array.array;
+
+public class PrintAllSubstringsOfGivenString {
+	
+	static void printAllString(String str) {
+		
+		int n = str.length();
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<=n;j++)
+			{
+				System.out.println(str.substring(i, j));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "ankit";
+		
+		printAllString(str);
+	}
+}
